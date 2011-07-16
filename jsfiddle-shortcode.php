74fe7e800f8fff77f6ee84cabeff9629992c16b5
Willington Vega <wvega@wvega.com>
2011-07-16 11:48:04 -0500

    First release.

---
+++ jsfiddle-shortcode.php
@@ -0,0 +1,43 @@
+<?php
+/*
+Plugin Name: JSFiddle Shortcode
+Plugin URI:
+Description: Allows to easily embed Fiddles using a small shortcode.
+Version: 0.1
+Author: Willington Vega
+Author URI: http://wvega.com/
+
+Copyright 2011 Willington Vega
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+function jsfiddle_shortcode_handler($attrs, $content) {
+    $tabs = array('result', 'js', 'css', 'html');
+
+    extract(shortcode_atts(array(
+        'url' => 'http://jsfiddle.net/wvega/UupFu/',
+        'height' => '300px',
+        'include' => join(',', $tabs)
+    ), $attrs));
+    
+    $include = array_intersect(split(',', $include), $tabs);
+    $url = trim($url, '/') . '/embedded/' . join(',', $include);
+
+    $html = '<iframe style="width: 100%; height: ' . $height . '" src="' . $url . '"></iframe>';
+
+    return $html;
+}
+
+add_shortcode('jsfiddle', 'jsfiddle_shortcode_handler');
---
+++ readme.txt
@@ -0,0 +1,44 @@
+=== JSFiddle Shortcode ===
+Contributors: wvega
+Donate link: http://wvega.com/
+Tags: jsfiddle
+Requires at least: 3.2.1
+Tested up to: 3.2.1
+Stable tag: 0.1
+
+Allows to easily embed Fiddles using a small shortcode.
+
+== Description ==
+
+# JSFiddle Shortcode
+
+I tested the plugin with WordPress 3.2.1 but it's so simple that it should work with previous versions without problems. If you happen to test it against older versions, please let me know how it goes.
+
+## Usage:
+
+`[jsfiddle url="http://jsfiddle.net/wvega/UupFu/3/" height="300px" include="result,html,js,css"]`
+
+## Result:
+
+`<iframe style="width: 100%; height: 300px" src="http://jsfiddle.net/wvega/UupFu/3/embedded/result,html,js,css"></iframe>`
+
+<iframe style="width: 100%; height: 300px" src="http://jsfiddle.net/wvega/UupFu/3/embedded/result,html,js,css"></iframe>
+
+== Installation ==
+
+1. Extract the plugin files to the `/wp-content/plugins/` directory.
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Use `[jsfiddle]` in your post content.
+
+== Frequently Asked Questions ==
+
+== Screenshots ==
+
+1. A JSFiddle embedded in a post using the plugin.
+
+== Changelog ==
+
+= 0.1 =
+First release.
+
+== Upgrade Notice ==
