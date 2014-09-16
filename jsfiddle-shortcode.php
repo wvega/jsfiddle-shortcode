236cf643cde6580ceecb03336685fd02e2ff3221
Willington Vega <wvega@wvega.com>
2014-09-16 10:24:32 -0500

    Fix PHP Deprecated notice.

--- jsfiddle-shortcode.php
+++ jsfiddle-shortcode.php
@@ -1,39 +1,34 @@
 <?php
-/*
-Plugin Name: JSFiddle Shortcode
-Plugin URI: http://wvega.com/492/jsfiddle-shortcode/
-Description: Allows to easily embed Fiddles using a small shortcode.
-Version: 0.1
-Author: Willington Vega
-Author URI: http://wvega.com/
 
-Copyright 2011 Willington Vega
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+/**
+ * JSFiddle Shortcode
+ *
+ * @link              https://github.com/wvega/jsfiddle-shortcode
+ * @since             1.0.0
+ * @package           Plugin_Name
+ *
+ * @wordpress-plugin
+ * Plugin Name:       JSFiddle Shortcode
+ * Plugin URI:        http://wvega.com/492/jsfiddle-shortcode/
+ * Description:       Allows to easily embed Fiddles using a small shortcode.
+ * Version:           1.0.0
+ * Author:            Willington Vega
+ * Author URI:        http://wvega.com/
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ */
 
 function jsfiddle_shortcode_handler($attrs, $content) {
-    $tabs = array('result', 'js', 'css', 'html');
+    $accepted_tabs = array('result', 'js', 'css', 'html');
 
     extract(shortcode_atts(array(
         'url' => 'http://jsfiddle.net/wvega/UupFu/',
         'height' => '300px',
-        'include' => join(',', $tabs)
+        'include' => implode( ',', $accepted_tabs ),
     ), $attrs));
-    
-    $include = array_intersect(split(',', $include), $tabs);
-    $url = trim($url, '/') . '/embedded/' . join(',', $include);
+
+    $include = array_intersect( explode( ',', $include ), $accepted_tabs );
+    $url = trim( $url, '/' ) . '/embedded/' . implode( ',', $include );
 
     $html = '<iframe style="width: 100%; height: ' . $height . '" src="' . $url . '"></iframe>';
 
